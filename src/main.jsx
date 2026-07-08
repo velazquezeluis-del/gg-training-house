f6a324e408f1010de3b23a3bfd16b722bd4fe81a
velazquezeluis-del <velazquez.e.luis@gmail.com>
2026-07-07 23:47:42 -0300

    Add files via upload
    Primera versión

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import GGApp from './GGTrainingHouse'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <GGApp />
+  </React.StrictMode>
+)

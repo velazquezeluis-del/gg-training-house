7e20fe09c8b16c7ea5790be38f2052baa4f834d5
velazquezeluis-del <velazquez.e.luis@gmail.com>
2026-07-14 19:05:46 -0300

    Delete src directory

--- src/main.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import GGApp from './GGTrainingHouse'
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <GGApp />
-  </React.StrictMode>
-)
